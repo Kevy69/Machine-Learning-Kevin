d4f4dc6feda9c28b07563fae53fcd70b50d49d89
Kevin Abrahamsson <kevin.abrahamsson@iths.se>
2023-03-24 20:23:12 +0100

    .

---
+++ Lab/ML-Helper.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+
+def train_val_test_split():
+    
